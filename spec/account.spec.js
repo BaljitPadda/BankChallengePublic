d9971a6755af9a43491b2acfc78ba7ca2e159a18
baljitpadda <baljitpadda@hotmail.co.uk>
2023-06-20 22:20:01 +0100

    10 tests passed.

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -14,6 +14,21 @@ describe('Account Class Tests', () => {
     }),
 
 
+        it("should be able to get the bank account balance", () => {
+
+            // Arrange
+            let account1 = new Account();
+            let expected = 0;
+
+            // Act
+            let actual = account1.getBalance();
+
+            // Assert
+            expect(actual).toEqual(expected)
+
+        }),
+
+
         it("should have a method that allows you to deposit a sum of money into the bank account", () => {
 
             // Arrange
@@ -49,19 +64,111 @@ describe('Account Class Tests', () => {
         }),
 
 
-        it("should be able to get the bank account balance", () => {
+        it("should ensure that you cannot deposit in a negative value", () => {
 
             // Arrange
             let account1 = new Account();
-            let expected = 0;
+            let sum = -50;
 
             // Act
-            let actual = account1.getBalance();
-
             // Assert
-            expect(actual).toEqual(expected)
+            expect(
+                function () {
+                    account1.deposit(sum);
+                }).toThrowError("Deposit sum must be a positive number greater than 0. Deposit sum cannot be null, undefined, negative etc.");
 
-        })
+
+        }),
+
+
+        it("should ensure that you cannot withdraw a negative value", () => {
+
+            // Arrange
+            let account1 = new Account();
+            let sum = -50;
+
+            // Act
+            // Assert
+            expect(
+                function () {
+                    account1.withdraw(sum);
+                }).toThrowError("Withdraw sum must be a positive number greater than 0. Withdraw sum cannot be null, undefined, negative etc.");
+
+
+        }),
+
+
+        it("should ensure that the sum to deposit in to the bank account is not undefined", () => {
+
+            // Arrange
+            let account1 = new Account();
+            let sum = undefined;
+
+            // Act
+            // Assert
+            expect(
+                function () {
+                    account1.deposit(sum);
+                }).toThrowError("Deposit sum must be a positive number greater than 0. Deposit sum cannot be null, undefined, negative etc.");
+
+
+
+        }),
+
+
+        it("should ensure that the sum to withdraw from the bank account is not undefined", () => {
+
+            // Arrange
+            let account1 = new Account();
+            let sum = undefined;
+
+            // Act
+            // Assert
+            expect(
+                function () {
+                    account1.withdraw(sum);
+                }).toThrowError("Withdraw sum must be a positive number greater than 0. Withdraw sum cannot be null, undefined, negative etc.");
+
+
+
+        }),
+
+
+        it("should ensure that the sum to deposit in to the bank account is not null", () => {
+
+            // Arrange
+            let account1 = new Account();
+            let sum = null;
+
+            // Act
+            // Assert
+            expect(
+                function () {
+                    account1.deposit(sum);
+                }).toThrowError("Deposit sum must be a positive number greater than 0. Deposit sum cannot be null, undefined, negative etc.");
+
+
+        }),
+
+
+        it("should ensure that the sum to withdraw from the bank account is not null", () => {
+
+            // Arrange
+            let account1 = new Account();
+            let sum = null;
+
+            // Act
+            // Assert
+            expect(
+                function () {
+                    account1.withdraw(sum);
+                }).toThrowError("Withdraw sum must be a positive number greater than 0. Withdraw sum cannot be null, undefined, negative etc.");
+
+
+        });
+
+
+
 
 });
 
--- src/Account.js
+++ src/Account.js
@@ -3,11 +3,16 @@ class Account {
     #balance = 0;
 
     deposit(sum) {
-        this.#balance += sum;
+        if (sum > 0) {
+            this.#balance += sum;
+        } else throw new Error("Deposit sum must be a positive number greater than 0. Deposit sum cannot be null, undefined, negative etc.");
     }
 
+
     withdraw(sum) {
-        this.#balance -= sum;
+        if (sum > 0) {
+            this.#balance -= sum;
+        } else throw new Error("Withdraw sum must be a positive number greater than 0. Withdraw sum cannot be null, undefined, negative etc.")
     }
 
     getBalance() {
